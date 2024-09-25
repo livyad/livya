1d9f7caa34ed3ebb2ba6b2f064aa17b90f068bad
livyad <cakereallyscakereallys@gmail.com>
2024-09-25 11:42:21 -0300

    c

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-alert("livya dias")
 function soma(num1,num2) {
     return num1 + num2;
 }
 console.log (soma (3,4));
 
+function quadrado(num1 num2) {
+    return num1*num2
+}
 
+console.log(quadrado (2))
 
 
 
