6e4e48e088ef9fa0d6f1340e1afc268f7b56d76e
livyad <cakereallyscakereallys@gmail.com>
2024-09-25 12:13:04 -0300

    ;

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 function soma(num1,num2) {
     return num1 + num2;
 }
-console.log (soma (3,4));
+console.log (soma (5,2));
 
-function areaQuadrado  (num1) {
-    return  (num1**2)
+function areaQuadrado  (lado) {
+  return  (lado*lado);
 }
 
-console.log (areaQuadrado (4))
+console.log (areaQuadrado (4));
 
+let media=function (numero1numero2numero3) {
+return x+y+z/3;
+}
+  
 
-
+console.log()
 
 
 
