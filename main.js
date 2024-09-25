c719e9c6b874b7f242caa320e114faa70b87aeb5
livyad <cakereallyscakereallys@gmail.com>
2024-09-25 12:31:02 -0300

    ggg

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-function soma(num1,num2) {
-    return num1 + num2;
+let CalculodeMediaDoUsuario = function Mediazinha(num1, num2, num3) {
+    num1 = parseInt(prompt("Me dê o primeiro número"))
+    num2 = parseInt(prompt("Me dê o segundo número"))
+    num3 = parseInt(prompt("Me dê o terceiro número"))
+    return (num1+num2+num3)/3   
 }
-console.log (soma (5,2));
+console.log(CalculodeMediaDoUsuario())
 
-function areaQuadrado  (lado) {
-  return  (lado*lado);
+
+let Soma = function somaBasica(num1, num2) {
+    return num1+num2
 }
 
-console.log (areaQuadrado (4));
+console.log(Soma(2, 3))
 
-let media=function (numero1numero2numero3) {
-return x+y+z/3;
+let Quadrado = function areaQuadrado(num1) {
+    return num1*num1
 }
-  
 
-console.log()
-let nome=prompt('livya e eduardo')
-    document.write(livya e Eduardo)
-    parseInt(prompt('qual e a media?'));
-    console.log(nome(livya e eduardo));
+console.log(Quadrado(2))
 
+function Triangulo(num1, num2, num3) {
+    num3 = 2
+    return (num1*num2)/num3
+}
+
+console.log(Triangulo(2,3))
+
+let Media = function calculoMedia(num1, num2, num3) {
+    return (num1+num2+num3)/3
+}
+
+console.log(Media(2, 3, 10))
 
 
 
